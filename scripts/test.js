45f24791de0641642ae26bdcc13db1b0fb11fcab
Michael Grant <harmoneedj@gmail.com>
2017-06-06 15:38:09 +0100

    Exercise #2 Commit

---
+++ scripts/test.js
@@ -0,0 +1,26 @@
+/*
+	TestJS (test.js)
+	Author: Michael Grant
+	Created: 06/06/2017
+	
+*/
+
+var TestJS = function () {
+	
+	// *************************************************************
+	// PUBLIC METHODS
+	
+	var fnInit = function(){
+		// Init code here if needed . . .
+	}
+	
+	// *************************************************************
+	// PRIVATE METHODS
+	
+	// *************************************************************
+	// Return public methods container
+	return {
+		init: fnInit
+	};
+	
+}();
